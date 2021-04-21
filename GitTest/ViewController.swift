d448f35d797acc4961f1f5d0fde333fa7e241454
igor <igro.sia@yandex.ru>
2021-04-21 21:47:53 +0300

    add label

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var testLable: UILabel = {
+        var lable = UILabel()
+        lable.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        lable.text = "test"
+        lable.textAlignment = .center
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        return lable
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        createTestLable()
+       
         // Do any additional setup after loading the view.
+    }
+    
+    func createTestLable() {
+        view.addSubview(testLable)
+        testLable.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        testLable.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        testLable.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        testLable.heightAnchor.constraint(equalToConstant: 200).isActive = true
     }
 
 
